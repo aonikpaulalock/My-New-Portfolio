3fc56e3da8f4dc4f1c7127e8e46895d03305e911
aonikpaulalock <alockpaul9845@gmail.com>
2022-10-14 19:26:36 +0600

    aded banner part

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,7 +2,10 @@ import React from 'react';
 import "../../Pages/Styles/Banner.css"
 import image from "../../Asset/Banner/bannerImg.png"
 import shape from "../../Asset/Banner/Vector 1.png"
+import Typed from 'react-typed';
 import { Icon } from '@iconify/react';
+import { Link } from 'react-router-dom';
+const resume = 'https://drive.google.com/file/d/1Ls0I5jjYrqvZzedKyhrjwXuJufnigOa5/view?usp=sharing'
 const Banner = () => {
   return (
     <div className="Banner-container">
@@ -15,10 +18,28 @@ const Banner = () => {
                 <h1>Aonik Paul Alock</h1>
                 <img src={shape} alt="" className="image-place" />
               </div>
-              <h4>Junior Full Stack Developer</h4>
+              <Typed
+                strings={[
+                  'Junior Frontend Developer',
+                  'React Developer',
+                  'Javascript Developer',
+                  'Junior Full Stack Developer',
+                  'MERN Stack Developer'
+                ]}
+                typeSpeed={60}
+                backSpeed={50}
+                className="slider-heading"
+                loop
+              />
+
               <button className="resume-button d-flex justify-align-content-center align-items-center">
-                <Icon icon="bi:download" className="download-icon" />
-                Download Resume</button>
+              <a
+                href={resume}
+                target="_blank"
+                rel="noreferrer"
+                className="link-work">
+                <Icon icon="bi:download" className="download-icon" />Download Resume</a> 
+               </button>
             </div>
           </div>
           <div className="col-lg-6 d-flex justify-content-center align-items-center">
